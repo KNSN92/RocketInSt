4170d2d6148ca2f9b656283987a8d7383ff9fd2f
KNSN92 <kekenshin92@gmail.com>
2025-01-15 23:10:06 +0900

    hothix: searchParams type as a Promise

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -6,7 +6,7 @@ import Image from "next/image";
 export default async function SignIn({
   searchParams,
 }: {
-  searchParams: { callbackUrl: string };
+  searchParams: Promise<{ callbackUrl: string }>;
 }) {
   const { callbackUrl } = await searchParams;
   const session = await getServerSession(authConfig);
