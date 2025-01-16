45d319353a932f661f0eed215348952882284d0d
KNSN92 <kekenshin92@gmail.com>
2025-01-16 09:05:16 +0900

    hotfix: fix puzzling login behavior on vercel environment?

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,12 @@
 import { withAuth } from "next-auth/middleware";
 
 export default withAuth({
+    callbacks: {
+        authorized({ req, token }) {
+            if(token) return true;
+            return false;
+        }
+    },
     pages: {
         signIn: "/signin"
     }
