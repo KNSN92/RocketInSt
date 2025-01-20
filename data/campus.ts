dbcbcf5a2bd97c7749f4e9fee36eb514fbb070e4
KNSN92 <kekenshin92@gmail.com>
2025-01-21 03:00:12 +0900

    add: room plan seed data

--- data/campus.ts
+++ data/campus.ts
@@ -1,10 +1,10 @@
 
-const campuses = {
+const campuses: {[campus: string]: { name: string, plan: { x: number, y: number, w: number, h: number } }[]} = {
     "神戸三ノ宮キャンパス": [
-        "大広間",
-        "万里",
-        "秘密基地",
-        "7階",
+        {name: "大広間", plan: {x: 1, y: 0, w: 3, h: 2}},
+        {name: "万里", plan: {x: 2, y: 2, w: 2, h: 1}},
+        {name: "秘密基地", plan: {x: 0, y: 0, w: 1, h: 1}},
+        {name: "7階", plan: {x: 0, y: 4, w: 4, h: 2}},
     ]
 } as const
 
--- prisma/seed/campus.ts
+++ prisma/seed/campus.ts
@@ -4,6 +4,7 @@ import campuses from "@/data/campus";
 export default async function seed() {
     await prisma.campus.deleteMany()
     await prisma.room.deleteMany()
+    await prisma.roomPlan.deleteMany()
 
     Object.entries(campuses).forEach(async (value) => {
         const [campus, rooms] = value
@@ -15,7 +16,15 @@ export default async function seed() {
         await rooms.forEach(async (room) => {
             await prisma.room.create({
                 data: {
-                    name: room,
+                    name: room.name,
+                    roomPlan: {
+                        create: {
+                            x: room.plan.x,
+                            y: room.plan.y,
+                            w: room.plan.w,
+                            h: room.plan.h,
+                        }
+                    },
                     campus: {
                         connect: {
                             id: id
