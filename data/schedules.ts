6d78c6348656907c14fc68e79baeb093600eb46c
KNSN92 <kekenshin92@gmail.com>
2025-02-02 22:13:42 +0900

    change: schedules type as const

--- data/schedules.ts
+++ data/schedules.ts
@@ -22,63 +22,66 @@ export const ROW_JA: {[key in LessonPeriodType]: string} = {
     "FourthPeriod": "4限目",
     "FifthPeriod": "5限目",
     "SixthPeriod": "6限目",
-} as const
+} as const;
 
-type PeriodField = string[]
-type DayColumn = {[key in LessonPeriodType]: PeriodField}
-type WeekTable = {[key in DayOfWeekType]: DayColumn}
+type DayColumn = {
+    readonly [key in LessonPeriodType]: readonly string[];
+  };
+  type WeekTable = {
+    readonly [key in DayOfWeekType]: DayColumn;
+  };
 
 const monday: DayColumn = {
-    "FirstPeriod": ["サークル活動", "エキスパートプログラム", "自主学習"],
-    "SecondPeriod": ["サークル活動", "エキスパートプログラム", "自主学習"],
-    "ThirdPeriod": ["進路授業1,2年次", "進路準備3年(進学)", "進路準備3年(就職)"],
-    "FourthPeriod": ["リベラルアーツ基礎(人文科学)", "自主学習"],
-    "FifthPeriod": ["実践英語", "プログラミングTA講座", "クリエイティブ検定", "自主学習"],
-    "SixthPeriod": ["一般教養(数学)", "自主学習"],
-} as const
+    "FirstPeriod": ["サークル活動", "エキスパートプログラム", "自主学習"] as const,
+    "SecondPeriod": ["サークル活動", "エキスパートプログラム", "自主学習"] as const,
+    "ThirdPeriod": ["進路授業1,2年次", "進路準備3年(進学)", "進路準備3年(就職)"] as const,
+    "FourthPeriod": ["リベラルアーツ基礎(人文科学)", "自主学習"] as const,
+    "FifthPeriod": ["実践英語", "プログラミングTA講座", "クリエイティブ検定", "自主学習"] as const,
+    "SixthPeriod": ["一般教養(数学)", "自主学習"] as const,
+} satisfies DayColumn;
 
 const tuesday: DayColumn = {
-    "FirstPeriod": ["リベラルアーツ応用", "自主学習"],
-    "SecondPeriod": ["リベラルアーツ応用", "自主学習"],
-    "ThirdPeriod": ["キャンパス授業"],
-    "FourthPeriod": ["総合型選抜対策(応用)", "自主学習"],
-    "FifthPeriod": ["中国語初級", "自主学習"],
-    "SixthPeriod": ["中国語初級", "自主学習"],
-} as const
+    "FirstPeriod": ["リベラルアーツ応用", "自主学習"] as const,
+    "SecondPeriod": ["リベラルアーツ応用", "自主学習"] as const,
+    "ThirdPeriod": ["キャンパス授業"] as const,
+    "FourthPeriod": ["総合型選抜対策(応用)", "自主学習"] as const,
+    "FifthPeriod": ["中国語初級", "自主学習"] as const,
+    "SixthPeriod": ["中国語初級", "自主学習"] as const,
+} satisfies DayColumn;
 
 const wednesday: DayColumn = {
-    "FirstPeriod": ["プロジェクトNα", "プロジェクトNβ", "自主学習"],
-    "SecondPeriod": ["プロジェクトNα", "プロジェクトNβ", "自主学習"],
-    "ThirdPeriod": ["総合型選抜対策(標準)", "自主学習"],
-    "FourthPeriod": ["リベラルアーツ基礎(社会科学)", "自主学習"],
-    "FifthPeriod": ["統計検定4級対策", "中国語中級", "中国語スピーキング講座", "自主学習"],
-    "SixthPeriod": ["ゼミナール", "中国語中級", "中国語スピーキング講座", "自主学習"],
-} as const
+    "FirstPeriod": ["プロジェクトNα", "プロジェクトNβ", "自主学習"] as const,
+    "SecondPeriod": ["プロジェクトNα", "プロジェクトNβ", "自主学習"] as const,
+    "ThirdPeriod": ["総合型選抜対策(標準)", "自主学習"] as const,
+    "FourthPeriod": ["リベラルアーツ基礎(社会科学)", "自主学習"] as const,
+    "FifthPeriod": ["統計検定4級対策", "中国語中級", "中国語スピーキング講座", "自主学習"] as const,
+    "SixthPeriod": ["ゼミナール", "中国語中級", "中国語スピーキング講座", "自主学習"] as const,
+} satisfies DayColumn;
 
 const thursday: DayColumn = {
-    "FirstPeriod": ["プロジェクトN応用", "自習"],
-    "SecondPeriod": ["プロジェクトN応用", "自習"],
-    "ThirdPeriod": ["キャンパス授業"],
-    "FourthPeriod": ["アート・デザイン", "自主学習"],
-    "FifthPeriod": ["アート・デザイン", "自主学習"],
-    "SixthPeriod": ["コミュニティ活動", "自主学習"],
-} as const
+    "FirstPeriod": ["プロジェクトN応用", "自習"] as const,
+    "SecondPeriod": ["プロジェクトN応用", "自習"] as const,
+    "ThirdPeriod": ["キャンパス授業"] as const,
+    "FourthPeriod": ["アート・デザイン", "自主学習"] as const,
+    "FifthPeriod": ["アート・デザイン", "自主学習"] as const,
+    "SixthPeriod": ["コミュニティ活動", "自主学習"] as const,
+} satisfies DayColumn;
 
-const friday: DayColumn = {
-    "FirstPeriod": ["プロジェクトNα", "プロジェクトNβ", "自主学習"],
-    "SecondPeriod": ["プロジェクトNα", "プロジェクトNβ", "自主学習"],
-    "ThirdPeriod": ["目標設定"],
-    "FourthPeriod": ["リベラルアーツ基礎(自然科学)", "自主学習"],
-    "FifthPeriod": ["英語検定2級対策", "英語討論講座", "自主学習"],
-    "SixthPeriod": ["キャンパス授業"],
-} as const
+const friday = {
+    "FirstPeriod": ["プロジェクトNα", "プロジェクトNβ", "自主学習"] as const,
+    "SecondPeriod": ["プロジェクトNα", "プロジェクトNβ", "自主学習"] as const,
+    "ThirdPeriod": ["目標設定"] as const,
+    "FourthPeriod": ["リベラルアーツ基礎(自然科学)", "自主学習"] as const,
+    "FifthPeriod": ["英語検定2級対策", "英語討論講座", "自主学習"] as const,
+    "SixthPeriod": ["キャンパス授業"] as const,
+} satisfies DayColumn;
 
-const table: WeekTable = {
+const table = {
     "Monday": monday,
     "Tuesday": tuesday,
     "Wednesday": wednesday,
     "Thursday": thursday,
     "Friday": friday
-} as const
+} satisfies WeekTable;
 
 export default table;
