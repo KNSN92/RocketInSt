c4d15acd04376f11021e29471bd6c021a974879a
KNSN92 <kekenshin92@gmail.com>
2025-02-03 14:01:03 +0900

    move: isTimeInRange from periodTimes

--- lib/time.ts
+++ lib/time.ts
@@ -14,3 +14,11 @@ export function getNowJSTTimeWithWeekday(): TimeWithWeekday {
     const nowJSTTime = new Date(new Date().getTime() + (JSTOffset * 3600 * 1000))
     return { weekday: nowJSTTime.getUTCDay(), hours: nowJSTTime.getUTCHours(), minutes: nowJSTTime.getUTCMinutes() }
 }
+
+export function isTimeInRange(time: Time, timeRange: TimeBetween) {
+    const totalMinutes = time.hours * 60 + time.minutes;
+    const startMinutes = timeRange.start.hours * 60 + timeRange.start.minutes;
+    const tmpEndMinutes = timeRange.end.hours * 60 + timeRange.end.minutes;
+    const endMinutes = startMinutes <= tmpEndMinutes ? tmpEndMinutes : tmpEndMinutes + 60 * 24
+    return startMinutes <= totalMinutes && totalMinutes <= endMinutes;
+}
