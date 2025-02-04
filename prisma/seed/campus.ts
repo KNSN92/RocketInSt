29115f12ef6e4ce10680a1dbc6e9343e91f2d3fe
KNSN92 <kekenshin92@gmail.com>
2025-02-05 00:00:40 +0900

    fix: null guard

--- prisma/seed/campus.ts
+++ prisma/seed/campus.ts
@@ -64,7 +64,7 @@ export default async function seed() {
                                     }
                                 }
                             })
-                        ).id;
+                        )?.id;
                         return {
                             id: lessonId
                         }
