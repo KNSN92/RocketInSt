5bbb57ba4625c2718a4301a0143035c5a14bacbf
KNSN92 <kekenshin92@gmail.com>
2025-02-04 22:02:07 +0900

    add: getNowJSTTimeAsMinutesWithWeekday

--- lib/time.ts
+++ lib/time.ts
@@ -2,17 +2,19 @@
 const JSTOffset = 9
 
 export type Time = { hours: number, minutes: number }
+export type TimeAsMinutes = number;
 export type TimeWithWeekday = Time & { weekday: number }
+export type TimeAsMinutesWithWeekday = { minutes: TimeAsMinutes, weekday: number }
 export type TimeBetween = { start: Time, end: Time }
-
-export function getNowJSTTime(): Time {
-    const nowJSTTime = new Date(new Date().getTime() + (JSTOffset * 3600 * 1000))
-    return { hours: nowJSTTime.getUTCHours(), minutes: nowJSTTime.getUTCMinutes() }
-}
 
 export function getNowJSTTimeWithWeekday(): TimeWithWeekday {
     const nowJSTTime = new Date(new Date().getTime() + (JSTOffset * 3600 * 1000))
     return { weekday: nowJSTTime.getUTCDay(), hours: nowJSTTime.getUTCHours(), minutes: nowJSTTime.getUTCMinutes() }
+}
+
+export function getNowJSTTimeAsMinutesWithWeekday(): TimeAsMinutesWithWeekday {
+    const nowJSTTime = new Date(new Date().getTime() + (JSTOffset * 3600 * 1000))
+    return { weekday: nowJSTTime.getUTCDay(), minutes: nowJSTTime.getUTCHours() * 60 + nowJSTTime.getUTCMinutes() }
 }
 
 export function isTimeInRange(time: Time, timeRange: TimeBetween) {
