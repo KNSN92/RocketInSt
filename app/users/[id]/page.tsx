e250b7b387cd3f8c4eef4cea0a097e5985f7f233
KNSN92 <kekenshin92@gmail.com>
2025-02-07 16:40:52 +0900

    update: show notFound page when user not found

--- app/users/[id]/page.tsx
+++ app/users/[id]/page.tsx
@@ -1,14 +1,20 @@
 import { prisma } from "@/prisma";
 import { notFound } from "next/navigation";
 
-export default async function UserInfo({ params }: { params: Promise<{ id: string }> }) {
-    const { id } = await params;
-    const user = await prisma.user.findUnique({
-        where: {
-            id: id
-        }
-    });
-    return (
-        <p>{user?.name}</p>
-    );
+export default async function UserInfo({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const { id } = await params;
+  const user = await prisma.user.findUnique({
+    where: {
+      id: id,
+    },
+  });
+  if (user) {
+    return <p>{user?.name}</p>;
+  } else {
+    return notFound();
+  }
 }
