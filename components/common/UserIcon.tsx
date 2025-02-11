224080a93f9c43164fdcb47f26293f1940db7f36
KNSN92 <kekenshin92@gmail.com>
2025-02-12 02:36:26 +0900

    add: UserIcon.tsx

---
+++ components/common/UserIcon.tsx
@@ -0,0 +1,51 @@
+import Image from "next/image";
+import { CSSProperties } from "react";
+
+export function UserIcon({
+  src,
+  width,
+  height,
+  className,
+}: {
+  src: string | null | undefined;
+  width: number;
+  height: number;
+  className?: HTMLDivElement["className"];
+}) {
+  if (src) {
+    return (
+      <Image
+        src={src}
+        alt="icon"
+        width={width}
+        height={height}
+        loading="lazy"
+        className={`rounded-full ${className}`}
+      />
+    );
+  } else {
+    return (
+      <UnknownUserIcon
+        className={className}
+        style={{ width: `${width}px`, height: `${height}px` }}
+      />
+    );
+  }
+}
+
+export function UnknownUserIcon({
+  className,
+  style,
+}: {
+  className?: HTMLDivElement["className"];
+  style?: CSSProperties;
+}) {
+  return (
+    <div
+      className={`${className} flex items-center justify-center text-4xl font-bold text-black bg-gray-400 rounded-full`}
+      style={style}
+    >
+      ?
+    </div>
+  );
+}
