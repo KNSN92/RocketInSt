feebc3cabf2077474d72d321dad1d0ad7d06da78
KNSN92 <kekenshin92@gmail.com>
2025-02-12 21:36:11 +0900

    move: Separate server actions from non-server actions

---
+++ lib/userdata.ts
@@ -0,0 +1,51 @@
+"use server";
+import authConfig from "@/auth.config";
+import { prisma } from "@/prisma";
+import { Prisma } from "@prisma/client";
+import { getServerSession } from "next-auth";
+
+export async function fetchUserId() {
+    return (await getServerSession(authConfig))?.user?.id || undefined;
+}
+
+export async function fetchUserCampusId(userId: string) {
+    return (await prisma.user.findUnique({ where: { id: userId }}))?.campusId || undefined;
+}
+
+export async function isUserCampusRegistered(userId: string) {
+    return (await fetchUserCampusId(userId)) !== undefined;
+}
+
+export async function userExist(userId: string) {
+    return (await prisma.user.findUnique({ where: { id: userId } })) !== undefined;
+}
+
+export async function fetchUser<T extends Prisma.UserSelect>(
+    userId: string,
+    select?: T | undefined
+): Promise<Prisma.UserGetPayload<{ select: T }> | undefined> {
+    if(select) return await prisma.user.findUnique({ where: { id: userId }, select: select }) || undefined;
+    return undefined;
+}
+
+export async function isUserFriend(myUserId: string, friendUserId: string) {
+    const following = (
+        await prisma.user.findUnique({
+        where: {
+            id: myUserId,
+        },
+        select: {
+            _count: {
+            select: {
+                following: {
+                where: {
+                    id: friendUserId,
+                },
+                },
+            },
+            },
+        },
+        })
+    )?._count.following;
+    return following !== undefined && following > 0;
+}
