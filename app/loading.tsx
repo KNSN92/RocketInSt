d0ca48d4a51a2242c92e1d3d1877422d97658d1f
KNSN92 <kekenshin92@gmail.com>
2025-02-17 01:50:54 +0900

    update: Replace “Loading...” on the Loading screen with Typewriter.

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,10 +1,15 @@
+"use client";
+
 import { RocketInStLoadingLogo } from "@/components/common/RocketInStLogos";
+import { Typewriter } from "react-simple-typewriter";
 
 export default function Loading() {
   return (
     <div className="flex h-main-content w-screen flex-col items-center justify-center">
       <RocketInStLoadingLogo className="w-[min(10vw 10vh)]" />
-      <div className="text-2xl">Loading...</div>
+      <div className="text-2xl">
+        <Typewriter words={["Loading..."]} cursor={true} loop={true} />
+      </div>
     </div>
   );
 }
