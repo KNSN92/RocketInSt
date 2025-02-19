0001a83ac2e9b78f21918ab12483a717e8cf70ad
KNSN92 <kekenshin92@gmail.com>
2025-02-20 01:36:14 +0900

    change: Changed the overall background and text colors to be managed by the tailwindcss class.

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,7 +37,7 @@ export default async function RootLayout({
   return (
     <html lang="ja">
       <body
-        className={`min-h-screen w-screen antialiased ${SawarabiGothicFont.className}`}
+        className={`min-h-screen w-screen bg-[var(--color-white)] text-[var(--color-black)] antialiased ${SawarabiGothicFont.className}`}
       >
         <NextAuthProvider>
           <Header />
