5efee6752aa8ed45e023ab5347df842bc2035d82
KNSN92 <kekenshin92@gmail.com>
2025-02-20 16:31:41 +0900

    update: Changed the theme button icon so that it becomes a Solid icon when pressed and an Outline icon when not pressed.

--- components/common/ThemeButton.tsx
+++ components/common/ThemeButton.tsx
@@ -2,18 +2,29 @@
 
 import { useEffect, useState } from "react";
 
-import SunIcon from "@heroicons/react/24/outline/SunIcon";
-import MoonIcon from "@heroicons/react/24/outline/MoonIcon";
-import ComputerDesktopIcon from "@heroicons/react/24/outline/ComputerDesktopIcon";
+import OutlineSunIcon from "@heroicons/react/24/outline/SunIcon";
+import OutlineMoonIcon from "@heroicons/react/24/outline/MoonIcon";
+import OutlineComputerDesktopIcon from "@heroicons/react/24/outline/ComputerDesktopIcon";
+import SolidSunIcon from "@heroicons/react/24/solid/SunIcon";
+import SolidMoonIcon from "@heroicons/react/24/solid/MoonIcon";
+import SolidComputerDesktopIcon from "@heroicons/react/24/solid/ComputerDesktopIcon";
 import clsx from "clsx";
 
 export default function ThemeButton() {
 
-	const rawTheme = typeof window !== "undefined" ? localStorage.theme : undefined;
-	const [theme, setTheme] = useState<"light" | "dark" | undefined>(rawTheme === "undefined" ? undefined : rawTheme);
+	const [theme, setTheme] = useState<"light" | "dark" | undefined>("light");
 
 	useEffect(() => {
-		if(typeof window !== "undefined") localStorage.theme = theme;
+		if(typeof window !== "undefined") {
+			const rawTheme = typeof window !== "undefined" ? localStorage.theme : undefined;
+			setTheme(rawTheme === "undefined" ? undefined : rawTheme);
+		};
+	}, [])
+	
+	useEffect(() => {
+		if(typeof window !== "undefined") {
+			localStorage.theme = theme;
+		};
 		document.documentElement.classList.toggle(
 			"dark",
 			theme === "dark" ||
@@ -25,17 +36,20 @@ export default function ThemeButton() {
 			<button className={clsx("w-fit h-fit p-2 flex gap-1 items-center rounded-l-full", theme === "light" && "bg-blue-800")}
 				onClick={() => setTheme("light")}
 			>
-				<SunIcon height={24} />
+				<OutlineSunIcon height={24} className={theme !== "light" ? "inline" : "hidden"} />
+				<SolidSunIcon height={24} className={theme === "light" ? "inline" : "hidden"} />
 			</button>
 			<button className={clsx("w-fit h-fit p-2 flex gap-1 items-center", theme === "dark" && "bg-blue-800")}
 				onClick={() => setTheme("dark")}
 			>
-				<MoonIcon height={24} />
+				<OutlineMoonIcon height={24} className={theme !== "dark" ? "inline" : "hidden"} />
+				<SolidMoonIcon height={24} className={theme === "dark" ? "inline" : "hidden"} />
 			</button>
 			<button className={clsx("w-fit h-fit p-2 flex gap-1 items-center rounded-r-full", theme === undefined && "bg-blue-800")}
 				onClick={() => setTheme(undefined)}
 			>
-				<ComputerDesktopIcon height={24} />
+				<OutlineComputerDesktopIcon height={24} className={theme !== undefined ? "inline" : "hidden"} />
+				<SolidComputerDesktopIcon height={24} className={theme === undefined ? "inline" : "hidden"} />
 			</button>
 		</div>
 	);
